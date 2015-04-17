a30699b8b33d3246c7c63ef4004217c519444591
costolo <ben.costolo@gmail.com>
2015-04-17 18:16:22 -0400

    add question and answer routes

--- app/controllers/question.rb
+++ app/controllers/question.rb
@@ -2,3 +2,12 @@ get '/questions/:id' do |id|
   question = Question.find(id)
   erb :_question, layout: false, locals: {question: question}
 end
+
+get '/question' do
+  
+end
+
+post '/question' do
+
+end
+
