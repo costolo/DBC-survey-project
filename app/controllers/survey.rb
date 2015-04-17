7d212925a0bb518d463609953b155967bd43cc6b
Sabron13 <kurt.schnelker@gmail.com>
2015-04-17 15:35:36 -0400

    starts CRUD routes for survey

---
+++ app/controllers/survey.rb
@@ -0,0 +1,20 @@
+get '/surveys' do
+  @surveys = Survey.all
+  erb :'surveys/index'
+end
+
+get '/surveys/new' do
+  erb :'surveys/new'
+end
+
+get '/surveys/:id' do |id|
+  @survey = Survey.find(id)
+  erb :'/surveys/show'
+end
+
+post '/surveys' do
+  Survey.create(params[:survey])
+  redirect '/surveys'
+end
+
+put
