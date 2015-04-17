eea9cd45d397227e9f99734d90e5be091eb3319d
costolo <ben.costolo@gmail.com>
2015-04-17 16:04:20 -0400

    Update seed file to include more surveys

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,13 @@
+require 'faker'
+
 User.create(name: "user", password: "user")
 Survey.create(title: "survey", description: "survey", user_id: 1)
 Question.create(content: "questions", survey_id: 1)
 Answer.create(content: "answer", question_id: 1)
 SurveyUser.create(user_id: 1, survey_id: 1)
 AnswerUser.create(user_id: 1, answer_id: 1)
+
+10.times do
+  Survey.create(title: Faker::Company.name, description: Faker::Company.catch_phrase, user_id: 1)
+end 
+  
