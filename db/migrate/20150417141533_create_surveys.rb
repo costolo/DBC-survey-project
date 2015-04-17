0121161bc24b48a311d1badce8448488212d32d4
Ben Costolo <ben.costolo@gmail.com>
2015-04-17 14:52:11 -0400

    Add migrations

--- db/migrate/20150417141533_create_surveys.rb
+++ db/migrate/20150417141533_create_surveys.rb
@@ -1,4 +1,11 @@
 class CreateSurveys < ActiveRecord::Migration
   def change
+    create_table :surveys do |t|
+      t.string :title, null: false
+      t.text :description, null: false
+      t.references :user, null: false
+
+      t.timestamps
+    end
   end
 end
--- db/migrate/20150417141601_create_answer_users.rb
+++ db/migrate/20150417141601_create_answer_users.rb
@@ -1,4 +1,10 @@
 class CreateAnswerUsers < ActiveRecord::Migration
   def change
+    create_table :answer_users do |t|
+      t.references :answer, null: false
+      t.references :user, null: false
+
+      t.timestamps
+    end
   end
 end
