101f1c8b949cc1d9c7ee3db09e4b68cf88413d29
Sabron13 <kurt.schnelker@gmail.com>
2015-04-17 16:43:18 -0400

    auth routes for log in and sign up

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -3,9 +3,25 @@ get '/' do
 end
 
 get '/login' do
-  
+	erb :login
 end
 
-get '/signup' do
-  
+post '/login' do
+	user = User.find_by(name: params[:name])
+	if user.authenticate(params[:password])
+		session[:user_id] = user.id
+		redirect '/surveys'
+	else
+		@signin_error = "Bad Login"
+		redirect '/login'
+	end
+end
+
+post '/signup' do
+	user = User.create(params[:user])
+	session[:user_id] = user.id
+end
+
+get '/logout' do
+	session[:user_id] = nil
 end
