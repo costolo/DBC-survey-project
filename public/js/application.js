bdde6248655fe19954231ad8b5d7881efa4eac2c
suprfrye <tara.c.frye@gmail.com>
2015-04-17 19:36:21 -0400

    Add ajax call to take survey button event handler

--- public/js/application.js
+++ public/js/application.js
@@ -1,11 +1,26 @@
 $(document).ready(function() {
 
   $('#show').on('click', function(event) {
-    event.preventDefault;
-    // $('#instructions').toggle();
     $('#questions').toggle();
     var buttonLabel = $(this).text();
     console.log(buttonLabel);
     $(this).text(buttonLabel === "Show Survey" ? "Hide Survey" : "Show Survey");
+  });
+
+  $('#take').on('click', function(event) {
+    event.preventDefault;
+    $('#instructions').toggle();
+    $(this).prop('disabled', true);
+    $('#show').prop('disabled', true);
+    $('#questions').toggle(false);
+    var firstQuestionId = $('[name="question_id"]').val();
+
+    $.ajax({
+      url: '/questions/' + firstQuestionId,
+      type: 'GET',
+      dataType: 'html'
+    }).done(function(response) {
+      $('#question-display').html(response);
+    });
   });
 });
