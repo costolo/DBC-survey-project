9e7b4107e5f7dbd5005814e2c1aa138ac7a7b7f8
suprfrye <tara.c.frye@gmail.com>
2015-04-18 17:42:59 -0400

    Disable survey buttons on show of survey

--- public/js/application.js
+++ public/js/application.js
@@ -2,21 +2,20 @@ $(document).ready(function() {
 
   $('#show').on('click', function(event) {
     $('#questions').toggle();
+    $('.response').prop('disabled', true);
+    $('[type="radio"]').prop('disabled', true);
     var buttonLabel = $(this).text();
-    console.log(buttonLabel);
     $(this).text(buttonLabel === "Show Survey" ? "Hide Survey" : "Show Survey");
   });
 
   $('#take').on('click', function(event) {
-    // event.preventDefault;
     $('#instructions').toggle();
     $(this).prop('disabled', true);
     $('#show').prop('disabled', true);
     $('#questions').toggle(false);
-    // var firstQuestionId = $('[name="question_id"]').val();
 
     $.ajax({
-      url: '/question',// + firstQuestionId,
+      url: '/question',
       type: 'GET',
       dataType: 'html'
     }).done(function(response) {
@@ -24,5 +23,19 @@ $(document).ready(function() {
     });
   });
 
-  
+  $('#show-survey').on('submit', '.question-response', function(event) {
+    event.preventDefault();
+
+    var data = $('.question-response').serialize();
+    $.ajax({
+      url: '/answer_users',
+      type: 'POST',
+      data: data,
+      dataType: 'html'
+    }).done(function(response) {
+      console.log(response);
+      $('#question-display').html(response);
+    });
+  });
+
 });
