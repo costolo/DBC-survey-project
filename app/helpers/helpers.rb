8cb2abb43367336cfa7ce576c4d33fa8877b95b3
costolo <ben.costolo@gmail.com>
2015-04-18 14:58:02 -0400

    Add helper method to find SurveyUser objects

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,3 +1,7 @@
 def user_from_survey(survey)
   User.find(survey.user_id)
 end
+
+def user_has_taken_survey?(survey)
+  SurveyUser.find_by(survey_id: survey.id, user_id: current_user.user_id) ? true : false
+end
