bc1197663be7267f00faa80478a3fecac331ddc5
Sabron13 <kurt.schnelker@gmail.com>
2015-04-19 18:05:55 -0400

    refactoring and bug fix

--- app/helpers/stats.rb
+++ app/helpers/stats.rb
@@ -1,25 +1,12 @@
 def surveys_made_stats (survey)
-	all_q_and_a = []
+	all_q_and_a_stats = []
 	survey.questions.each do |question|
-		most_popular = []
-		all_answers = AnswerUser.where("question_id = ?", question.id)
+		all_q_and_a_stats << "#{question.content}"
+		all_answerusers = AnswerUser.where("question_id = ?", question.id)
 		question.answers.each do |answer|
-			answer_array = AnswerUser.where("question_id = ? AND answer_id = ?", question.id, answer.id)
-			if answer_array.count > most_popular.count
-				most_popular = answer_array
-				all_q_and_a << "#{question.content}, #{answer.content}: #{((most_popular.count.to_f/all_answers.count.to_f) * 100).round(2)} %"
-
-			elsif answer_array.count == most_popular.count
-				
-			else
-
-			end
+			answerusers = AnswerUser.where("question_id = ? AND answer_id = ?", question.id, answer.id)
+			all_q_and_a_stats << "#{answer.content}: #{((answerusers.count.to_f/all_answerusers.count.to_f) * 100).round(2)} %"
 		end
 	end
-	all_q_and_a
-end
-
-
-def surveys_taken_stats (survey)
-
+	all_q_and_a_stats
 end
--- public/js/user-survey.js
+++ public/js/user-survey.js
@@ -1,22 +1,17 @@
 $(document).ready(function() {
-<<<<<<< HEAD
-  $('.stats-button').on('submit', function(event) {
-    event.preventDefault();
-    var $target = $(event.target);
-    $target.hide();
-    $.ajax({
-       url: $target.attr('action'),
-       type: "GET",
-    }).done(function(response) {
-      console.log(response)
-      $('.survey-stats').append(response + "<br>")
-    })
-  })
-=======
-	$('.stats-button').on('submit', function(event) {
-		event.preventDefault();
-		var $target = $(event.target);
-		$('.survey-stats').toggle();
-	})
->>>>>>> adds survey data to surveys users have made, need to fix bug that displays all instead of the one you clicked on
-})
+	$('.survey-div').each(function() {
+		var stats = $(this).children('.survey-stats');
+
+		$(this).click(function(event) {
+			event.preventDefault();
+			stats.toggle();
+		});
+	});
+});
+
+
+	// $('.stats-button').on('submit', function(event) {
+	// 	event.preventDefault();
+	// 	var $target = $(event.target);
+	// 	$('.survey-stats').toggle();
+	// })
