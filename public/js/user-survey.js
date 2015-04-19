539bf151300a27643be19e5d55b99dfad8ea7490
costolo <ben.costolo@gmail.com>
2015-04-19 11:46:41 -0400

    Fix whitespace in user-survey.js

--- public/js/user-survey.js
+++ public/js/user-survey.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
-	$('.stats-button').on('submit', function(event) {
-		event.preventDefault();
-		var $target = $(event.target);
-		$target.hide();
-		$.ajax({
-					 url: $target.attr('action'),
-					 type: "GET",
-		}).done(function(response) {
-			$('.survey-stats').append(response + "<br>")
-		})
-	})
+  $('.stats-button').on('submit', function(event) {
+    event.preventDefault();
+    var $target = $(event.target);
+    $target.hide();
+    $.ajax({
+       url: $target.attr('action'),
+       type: "GET",
+    }).done(function(response) {
+      console.log(response)
+      $('.survey-stats').append(response + "<br>")
+    })
+  })
 })
