06df171dceafe3962e54ab31702276e7d44d8edd
suprfrye <tara.c.frye@gmail.com>
2015-04-20 09:02:57 -0400

    Add belongs to question association to answer user model

--- app/models/answer_user.rb
+++ app/models/answer_user.rb
@@ -1,6 +1,7 @@
 class AnswerUser < ActiveRecord::Base
   belongs_to :user
   belongs_to :answer
+  belongs_to :question
 
-  validates :user_id, :answer_id, presence: true
+  validates :user_id, :answer_id, :question_id, presence: true
 end
