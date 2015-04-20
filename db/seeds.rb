d26805bdabbbe8306e147ab73f37248c02f17f61
suprfrye <tara.c.frye@gmail.com>
2015-04-20 10:17:33 -0400

    Add user toni to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 require 'faker'
 
 User.create(name: "user", password: "user")
+User.create(name: 'toni', password: '1234')
 Survey.create(title: "survey", description: "survey", user_id: 1)
 Question.create(content: "questions", survey_id: 1)
 Answer.create(content: "answer", question_id: 1)
@@ -8,7 +9,7 @@ SurveyUser.create(user_id: 1, survey_id: 1)
 AnswerUser.create(user_id: 1, answer_id: 1, question_id: 1)
 
 10.times do
-  survey = Survey.create(title: Faker::Company.name, description: Faker::Company.catch_phrase, user_id: 1)  
+  survey = Survey.create(title: Faker::Company.name, description: Faker::Company.catch_phrase, user_id: 1)
 	 10.times do
 	 	question = Question.create(content: Faker::Lorem.words.join(' ')+"?", survey_id: survey.id)
 	 	4.times do
@@ -18,7 +19,7 @@ AnswerUser.create(user_id: 1, answer_id: 1, question_id: 1)
 	 		end
 	 	end
 	end
-end 
+end
 
 
 
